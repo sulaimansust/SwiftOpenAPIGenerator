79c3363163ae8c5d5f5bf0879559726479d42e86
Sulayman Md <sulaiman.sust@gmail.com>
2023-08-01 18:04:29 +0200

    Initial project

---
+++ OpenApiExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenApiExample
+//
+//  Created by Md Sulayman on 01.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenApiExample/OpenApiExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenApiExampleApp.swift
+//  OpenApiExample
+//
+//  Created by Md Sulayman on 01.08.23.
+//
+
+import SwiftUI
+
+@main
+struct OpenApiExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
